8468229bf31cedfbd7c8aabdc05d3c1cc9af19dd
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-10-17 10:24:41 -0400

    Comment DemoTest5

---
+++ src/project5/DemoTest5.java
@@ -0,0 +1,12 @@
+package project5;
+
+public class DemoTest5 {
+	
+	
+	public static void main ( String [] args) {
+		System.out.println ("Java is Fun ");
+		System.out.println ( "Test");
+		
+
+}
+}
